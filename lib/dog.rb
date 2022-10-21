05a450209eb80ce5bf515b52bf31712392973700
saidsoud72 <said.omar@student.moringaschool.com>
2022-10-21 07:00:19 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
+require "pry"
 class Dog
+    def name=(dog_name)
+      @this_dogs_name = dog_name
+    end
+
+    def name
+      @this_dogs_name
+    end
+  end
 
 end
+lassie = Dog.new
+lassie.name = "Lassie"
